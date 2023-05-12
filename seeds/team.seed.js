f245cccd071fc9693f949a8c62e4fdaf44759318
Daniel Fernandez <dfernandezcode@gmail.com>
2023-05-12 20:36:02 +0200

    team seed and reset addedd

--- seeds/team.seed.js
+++ seeds/team.seed.js
@@ -1,14 +1,6 @@
 const mongoose = require("mongoose");
 const { connect } = require("../db.js");
-const { Team } = require("../models/Team.js");
 const { resetTeam } = require("../utils/resetTeam");
-
-const teamList = [
-  { teamName: "ATLÉTICO DE MADRID", foundedIn: 1903, originCity: "MADRID" },
-  { teamName: "REAL MADRID", foundedIn: 1902, originCity: "MADRID" },
-  { teamName: "FC BARCELONA", foundedIn: 1899, originCity: "BARCELONA" },
-  { teamName: "PARIS SAINT GERMAIN", foundedIn: 1970, originCity: "PARIS" },
-];
 
 const teamSeed = async () => {
   try {
