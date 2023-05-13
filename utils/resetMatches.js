f2f6ddb22e39645482620cb2d88be76674ec80be
Daniel Fernandez <dfernandezcode@gmail.com>
2023-05-13 13:36:47 +0200

    errorfix done resetMetches

--- utils/resetMatches.js
+++ utils/resetMatches.js
@@ -4,7 +4,6 @@ const { Match } = require("../models/Match");
 // Utils import
 const { getRandomDate } = require("../utils/getRandomDate");
 const { shuffleArray } = require("../utils/shuffleArray");
-
 const resetMatches = async (teams) => {
   try {
     const teams = await Team.find();
@@ -13,50 +12,35 @@ const resetMatches = async (teams) => {
       return;
     }
     const shuffledTeams = shuffleArray(teams);
-
     const matches = [];
-
     // Generate matches for each round
     for (let i = 0; i < shuffledTeams.length - 1; i++) {
-      const roundMatches = [];
-
-      for (let j = 0; j < shuffledTeams.length / 2; j++) {
-        const localTeam = shuffledTeams[j];
-        const awayTeam = shuffledTeams[shuffledTeams.length - 1 - j];
-
-        // Create a new match object with a random date within the next 7 days
-        const match = {
-          localTeam: localTeam._id,
-          awayTeam: awayTeam._id,
-          localTeamGoal: 0,
-          awayTeamGoal: 0,
-          currentMatch: false,
-          matchDate: getRandomDate(new Date(), new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)),
-        };
-
-        roundMatches.push(match);
-      }
-
+      // for (let j = 0; j < shuffledTeams.length / 2; j++) {
+      //   const localTeam = shuffledTeams[j];
+      //   const awayTeam = shuffledTeams[shuffledTeams.length - 1 - j];
+      const localTeam = shuffledTeams[i];
+      const awayTeam = shuffledTeams[i + 1];
+      // Create a new match object with a random date within the next 7 days
+      const match = {
+        localTeam: localTeam.id,
+        awayTeam: awayTeam.id,
+        localTeamGoal: 0,
+        awayTeamGoal: 0,
+        currentMatch: false,
+        matchDate: getRandomDate(new Date(), new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)),
+      };
+      matches.push(match);
       // Rotate the teams to create different pairings for the next round
-      shuffledTeams.splice(1, 0, shuffledTeams.pop());
-
-      matches.push(roundMatches);
     }
-
     // Save the matches to the database
-    for (const roundMatches of matches) {
-      await mongoose.model("Match").insertMany(roundMatches);
-    }
-
-    console.log(`Created ${matches.length} rounds of matches`);
     await Match.collection.drop(); //  Esperamos a que borre los documentos de la colección matche de la BBDD.
     console.log("Borrados matches");
-    await Match.insertMany(matches); //  Esperamos a que inserte los nuevos documentos creados en la colección matche de la BBDD.
+    const documents = matches.map((match) => new Match(match));
+    await Match.insertMany(documents); //  Esperamos a que inserte los nuevos documentos creados en la colección matche de la BBDD.
     console.log("Creados matches correctamente");
   } catch (error) {
     console.error(error);
   }
   // Shuffle the teams to randomize match pairings
 };
-
 module.exports = { resetMatches };
