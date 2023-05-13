52251a5d03748e79748c2bb676cf7f83f08621ef
Daniel Fernandez <dfernandezcode@gmail.com>
2023-05-13 12:33:52 +0200

    added utils relations

---
+++ seeds/match.seed.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const { connect } = require("../db.js");
+const { resetMatches } = require("../utils/resetMatches.js");
+
+const matchSeed = async () => {
+  try {
+    await connect();
+    console.log("Tenemos conexión");
+    await resetMatches();
+  } catch (error) {
+    console.error("ERROR AL CONECTAR CON LA BBDD");
+    console.error(error);
+  } finally {
+    mongoose.disconnect();
+  }
+};
+
+matchSeed();
